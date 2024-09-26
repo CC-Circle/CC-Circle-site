bbeed44ba7a0c02882f778791a5248d63b36a275
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-09-26 20:57:20 +0900

    fix

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,23 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { faGithub, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="flex items-center justify-between">
       <Link href="/">
-        {/* <Logo num={150} /> */}
         <CCC />
       </Link>
       <SNSIcons />
     </div>
   );
-
-  // function Logo({ num: num }: Readonly<{ num: number }>) {
-  //   return <Image src="/logo.svg" alt="logo" width={num} height={num} />;
-  // }
 
   function CCC() {
     return <div className="text-7xl font-bold">CCC</div>;
@@ -26,8 +20,12 @@ export default function Header() {
   function SNSIcons() {
     return (
       <div>
-        <FontAwesomeIcon className="size-5 px-2" icon={faGithub} />
-        <FontAwesomeIcon className="size-5" icon={faXTwitter} />
+        <Link href="https://github.com/CC-Circle">
+          <FontAwesomeIcon className="size-5 px-2" icon={faGithub} />
+        </Link>
+        <Link href="https://x.com/CCC_AIT">
+          <FontAwesomeIcon className="size-5" icon={faXTwitter} />
+        </Link>
       </div>
     );
   }
