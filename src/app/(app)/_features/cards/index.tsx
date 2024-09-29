09cd0be266d67f5ee748d21acce9961353458aa9
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-09-29 13:01:24 +0900

    deploy test

--- src/app/(app)/_features/cards/index.tsx
+++ src/app/(app)/_features/cards/index.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import nextConfig from "@/../next.config.mjs";
 import games from "@/data/pj.json";
 
+const BASE_PATH = nextConfig.basePath || "";
+
 export default function Cards() {
-  const defaultImage = "/pj/crop/logo.png";
+  const defaultImage = `/${BASE_PATH}/pj/crop/logo.png`;
 
   return (
     <div className="grid grid-cols-2 gap-8 md:grid-cols-3">
@@ -12,7 +15,7 @@ export default function Cards() {
         <div className="flex items-center justify-center" key={index}>
           <Link href={`${game.slug}`}>
             <Image
-              src={game.crop || defaultImage}
+              src={`/${BASE_PATH}/game.crop` || defaultImage}
               alt={game.title}
               width={600}
               height={600}
